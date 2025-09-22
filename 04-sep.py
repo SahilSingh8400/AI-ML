0ae9976800dcae7c12c89353c280cbcb83f0fe99
Sahil Singh <sanju98099@gmail.com>
2025-09-22 11:33:37 +0530

    first commit

---
+++ 04-sep.py
@@ -0,0 +1,10 @@
+# a=(input('enter the name -- '))
+# b=(input('enter the surname -- '))
+# print(a,b)
+num=int(input("enter your number   "))
+if num>0:
+    print(num," is a positive number")
+elif num<0:
+    print(num," is num is negative")
+else:
+    print(num," is num is zero")
